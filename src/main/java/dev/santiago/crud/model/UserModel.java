75aa2231a59308a9b47f8499b5a928f61b5aa2fe
Santiago <santiagopacheco34@gmail.com>
2025-10-19 15:07:58 -0300

    feat: lombok annotations

--- src/main/java/dev/santiago/crud/model/UserModel.java
+++ src/main/java/dev/santiago/crud/model/UserModel.java
@@ -1,9 +1,17 @@
 package dev.santiago.crud.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity (name = "user")
 @Table (name = "tb_user")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class UserModel {
 
     @Id
@@ -17,27 +25,4 @@ public class UserModel {
     @JoinColumn(name = "work_id")
     private WorkModel work;
 
-    public UserModel() {
-    }
-
-    public UserModel(String name, String email) {
-        this.name = name;
-        this.email = email;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
