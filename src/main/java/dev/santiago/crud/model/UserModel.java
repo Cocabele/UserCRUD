86b7af0e5bf9e86adc5e68b7b9a7803d2ec8f625
Santiago <santiagopacheco34@gmail.com>
2025-10-19 12:20:43 -0300

    chore: configure user model

--- src/main/java/dev/santiago/crud/model/UserModel.java
+++ src/main/java/dev/santiago/crud/model/UserModel.java
@@ -1,4 +1,39 @@
 package dev.santiago.crud.model;
 
-public class User {
+import jakarta.persistence.*;
+
+@Entity (name = "user")
+@Table (name = "tb_user")
+public class UserModel {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+
+    public UserModel() {
+    }
+
+    public UserModel(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
